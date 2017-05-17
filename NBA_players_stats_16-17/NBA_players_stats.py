932e3ada4da408e232e592939ad7bb87f5eab804
Your Name <st5721688@gmail.com>
2017-05-17 14:45:24 +0300

    Merge 3 datasets into one

--- NBA_players_stats_16-17/NBA_players_stats.py
+++ NBA_players_stats_16-17/NBA_players_stats.py
@@ -4,4 +4,6 @@ per_game = pd.read_csv('NBA_players_stats_per_game.csv')
 per_100_poss = pd.read_csv('NBA_players_stats_per_100_possessions.csv')
 advanced = pd.read_csv('NBA_players_stats_advanced.csv')
 
-print(per_game.head(1), '\n',  per_100_poss.head(1), '\n', advanced.head(1))
+#two = pd.merge(per_game, per_100_poss, on='Rk')
+stats = pd.concat([per_game, per_100_poss, advanced], axis=1)
+print(stats.head())
