7c160db3d24c4e51699e1ad59c4bc326c48b59a2
KonstantinLastovski <st5721688@gmail.com>
2017-05-17 21:25:15 +0300

    Added cleaning amd transformation of data

--- NBA_players_stats_16-17/NBA_players_stats.py
+++ NBA_players_stats_16-17/NBA_players_stats.py
@@ -17,4 +17,26 @@ stats = stats.T.drop_duplicates().T
 # Droping raws with stats of players which was traded this season (Dataframe had raws with stats in old team,
 # in new team and total. We keep only total stats of these players)
 stats = stats.drop_duplicates(['Rk']).reset_index()
+
+#Finding indexes of raws and columns with NaN values
+#x = list(pd.isnull(stats).any(1).nonzero()[0])
+#y = list(pd.isnull(stats).any(0).nonzero()[0])
+#print(x, y)
+#xy = stats.loc[x]
+
+# Replace all of the NaN values with 0
+stats = stats.fillna(0)
+
+#Convert columns to numeric formats
+stats.apply(pd.to_numeric, errors='ignore')
+
+#Rename players to normal view
+list_player = []
+for i in stats.Player:
+    i = i.split("\\")[0]
+    list_player.append(i)
+stats.Player = list_player
+
 print(stats.dtypes)
+
+
