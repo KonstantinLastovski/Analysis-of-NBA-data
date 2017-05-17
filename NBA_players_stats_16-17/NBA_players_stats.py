064229e9f83ed9af87648c94378c33803fff5cd4
Your Name <st5721688@gmail.com>
2017-05-17 16:02:08 +0300

    Transform the dataframe

--- NBA_players_stats_16-17/NBA_players_stats.py
+++ NBA_players_stats_16-17/NBA_players_stats.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
+# Reading datasets
 per_game = pd.read_csv('NBA_players_stats_per_game.csv')
 per_100_poss = pd.read_csv('NBA_players_stats_per_100_possessions.csv')
 advanced = pd.read_csv('NBA_players_stats_advanced.csv')
 
-#two = pd.merge(per_game, per_100_poss, on='Rk')
+# Merging 3 dataframes into one
 stats = pd.concat([per_game, per_100_poss, advanced], axis=1)
-print(stats.head())
+
+# Droping empty columns
+stats = stats.drop(['Unnamed: 24', 'Unnamed: 29', 'Unnamed: 19'], axis=1)
+print(stats.columns)
+
+# Droping duplicated columns with same values
+stats = stats.T.drop_duplicates().T
+print(stats.columns)
